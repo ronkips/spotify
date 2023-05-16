3ae0c0ffaaecf22fa11edd80c90138aea522baea
ronkips <kipshillash@gmail.com>
2023-05-16 17:43:28 +0300

    Merge branch 'main' of https://github.com/ronkips/spotify into Hilla

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,21 +1,15 @@
 import React from "react";
 // import Card from "./Card";
 import Album from "./Album";
-
-
+import Home from "./Home";
 import Link from "next/link";
 import Card from "./Card";
-=======
-import Collections from "./Collections"
-import Home from "./Home";
-
 const App = () => {
   return (
     <div>
       {/* <Card /> */}
       <Home />
       <Album />
-      {/* <Collections /> */}
     </div>
   );
 };
