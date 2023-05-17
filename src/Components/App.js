a1a7dcccd260e642156b81cd4eaed15d4f6b7710
ngunjiri <owen2ngunjiri@gmail.com>
2023-05-17 12:04:43 +0300

    update

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,12 +4,14 @@ import Album from "./Album";
 import Home from "./Home";
 import Link from "next/link";
 import Card from "./Card";
+import Collections from "./Collections";
 const App = () => {
   return (
     <div>
       {/* <Card /> */}
-      <Home />
-      <Album />
+      {/* <Home /> */}
+      {/* <Album /> */}
+      <Collections />
     </div>
   );
 };
